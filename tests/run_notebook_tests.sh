99302a2d5d75d5220dbe8eb78606853db4178b7a
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-02-26 18:41:27 +0100

    temporarily deactivate one notebook test

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -1,6 +1,8 @@
-# TODO make tutorial_dev_spectral_libraries.ipynb work
+#!/bin/bash
 
+# Run the notebook tests.
 
-ALL_NBS=$(find ../notebooks -name "*.ipynb")
+# TODO enable also 03b_basic_workflow.ipynb
+ALL_NBS=$(find ../notebooks -name "*.ipynb" | grep -v "03b_basic_workflow.ipynb")
 
 python -m pytest --nbmake $(echo $ALL_NBS)
