03d43063b75a7e1f4225ff18afa2a18341f4261b
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-05-29 12:43:25 +0200

    [Test] Update location of notebooks

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -5,6 +5,6 @@
 export IS_PYTEST_RUN=True
 
 # TODO enable also 03b_basic_workflow.ipynb
-ALL_NBS=$(find ../notebooks -name "*.ipynb" | grep -v "03b_basic_workflow.ipynb")
+ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v "03b_basic_workflow.ipynb")
 
 python -m pytest --nbmake $(echo $ALL_NBS)
