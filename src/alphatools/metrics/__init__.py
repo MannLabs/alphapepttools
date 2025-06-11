c8cb7b06055b7cc6a9b4d20443d06dad5ba808fb
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-06-11 22:51:02 +0100

    [API] Expose PMAD

--- src/alphatools/metrics/__init__.py
+++ src/alphatools/metrics/__init__.py
@@ -1,3 +1,6 @@
 """Metrics for the quality assessment of the analysis"""
 
+from ._pmad import pmad
 from ._principal_component_regression import principal_component_regression
+
+__all__ = ["pmad", "principal_component_regression"]
