5ed43e12b39eb2702569969f1af65022e69db51f
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-06-25 11:19:56 +0200

    [Tests] Add integration tests for DIANN output

--- tests/integration/test_protein_group.py
+++ tests/integration/test_protein_group.py
@@ -17,7 +17,7 @@ def test_pg_alphadia_110(tmpdir):
 
     file_path = DataShareDownloader(url=url, output_dir=tmpdir).download()
 
-    adata = read_pg_matrix(file_path=file_path)
+    adata = read_pg_matrix(file_path=file_path, sample_name="sample_id")
 
     _assert_reference_df_equal(adata.to_df(), "pg_alphadia110", check_psf_df_columns=False)
 
@@ -30,6 +30,29 @@ def test_pg_rosenberger2023(tmpdir):
 
     file_path = DataShareDownloader(url=url, output_dir=tmpdir).download()
 
-    adata = read_pg_matrix(file_path=file_path)
+    adata = read_pg_matrix(file_path=file_path, sample_name="sample_id")
 
     _assert_reference_df_equal(adata.to_df(), "pg_rosenberger2023", check_psf_df_columns=False)
+
+
+def test_pg_diann(tmpdir):
+    """Test creating anndata from DIANN protein group table using data by
+    Shani Ben-Moshe, 2025."""
+    # Create directly from files
+    url = "https://datashare.biochem.mpg.de/s/m8q3Lg2ppAWKCft"
+
+    file_path = DataShareDownloader(url=url, output_dir=tmpdir).download()
+
+    adata = read_pg_matrix(
+        file_path=file_path,
+        sample_name="sample_id",
+        feature_metadata_index=[
+            0,
+            1,
+            2,
+            3,
+        ],
+        feature_name=["pg", "protein_names", "genes", "first_protein_description"],
+    )
+
+    _assert_reference_df_equal(adata.to_df(), "pg_diann", check_psf_df_columns=False)
