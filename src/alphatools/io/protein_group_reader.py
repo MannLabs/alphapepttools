56ede2b5bc4f948980e0deec27a00f94beb92320
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-06-25 11:01:06 +0200

    [Refactor, Feature] Refactor function so that it is compatible with DIANN reports

--- src/alphatools/io/protein_group_reader.py
+++ src/alphatools/io/protein_group_reader.py
@@ -4,16 +4,20 @@ import anndata as ad
 import pandas as pd
 
 
+def _validate_row_type(row_type: Literal["observations", "features"]) -> None:
+    """Raise KeyError if row type is not one of expected values"""
+    if row_type not in ("features", "observations"):
+        raise KeyError("`row_type` must be one of `features` or `observations`")
+
+
 def read_pg_matrix(
     file_path: str,
-    sample_name: str = "sample_id",
-    feature_name: str = "pg",
-    *,
-    set_index: bool = True,
-    row_type: Literal["observation", "features"] = "features",
+    row_type: Literal["observations", "features"] = "features",
     separator: str = "\t",
-    index_col: str | int = 0,
-    header: str | int = 0,
+    sample_metadata_index: str | int | list[str] | list[int] = 0,
+    feature_metadata_index: str | int | list[str] | list[int] = 0,
+    sample_name: str | list[str] | None = None,
+    feature_name: str | list[str] | None = None,
     **kwargs,
 ) -> ad.AnnData:
     r"""Convert a protein group table stored in text format to :class:`anndata.AnnData`
@@ -22,22 +26,23 @@ def read_pg_matrix(
     ----------
     file_path
         Path to text file of shape N (samples) x F (features). Expects observations (e.g. cells, samples) in rows
-        and features (protein groups) in columns
-    sample_name
-        Column name of samples in `anndata.AnnData.obs` attribute (defaults to `sample_id`)
-    feature_name
-        Column name of features in `anndata.AnnData.var` attribute (defaults to `pg`)
-    set_index
-        Whether to set the sample names and feature names as indices in the :class:`anndata.AnnData` object in addition to the respective columns
-        If False uses default initialization with numeric indices.
+        and features (protein groups) in columns.
     row_type
         Whether features or observations are stored as rows. Per default, uses `row_type="features"`.
     separator
-        File separator. Defaults to a tab-separated file
-    index_col
-        Index column. Defaults to first column in protein group table
-    header
-        Indexing row. Defaults to first row in protein group table
+        File separator. Defaults to a tab-separated file.
+    sample_metadata_index
+        Indices of metadata for samples (either names or integer indices). If the `row_type` is "features" uses columns, if the `row_type` is "observations" uses rows.
+        Defaults to the first index.
+    feature_metadata_index
+        Indices of metadata for features (either names or integer indices). If the `row_type` is "features" uses rows, if the `row_type` is "observations" uses columns
+        Defaults to the first column.
+    sample_name
+        Column names for samples in `anndata.AnnData.obs` attribute. Must be the same length as `sample_metadata_index`.
+        Defaults to the original name of the sample metadata.
+    feature_name
+        Column names fors features in `anndata.AnnData.var` attribute. Must be the same length as `feature_metadata_index`.
+        Defaults to the original names of the feature metadata.
     **kwargs
         Keyword arguments passed to :func:`pandas.read_csv`
 
@@ -46,14 +51,14 @@ def read_pg_matrix(
     :class:`anndata.AnnData`
         AnnData object with N observations and F features.
 
-            - .obs Contains content of df.index
-            - .var contains content of df.columns
+            - .obs Contains content of the rows/columns indicated in sample_metadata_index
+            - .var contains content of the rows/columns indicated in sa_metadata_index
 
     Example
     -------
 
     Here we showcase the default behaviour of the reader. A (features x observations) protein group matrix
-    gets converted to an :class:`anndata.AnnData` object.
+    is converted into an :class:`anndata.AnnData` object.
 
     .. code-block:: python
 
@@ -70,21 +75,21 @@ def read_pg_matrix(
            P2        3   4   5  ...
            P3        6   7   8  ...
 
-        adata = at.io.read_pg_matrix(pg_matrix_path)
+        adata = at.io.read_pg_matrix(pg_matrix_path, sample_name = "sample_id")
         adata
         > AnnData object with n_obs x n_vars = 3 x 3
           obs: 'sample_id'
-          var: 'pg'
+          var: 'proteins'
         adata.obs
         >       sample_id
             A   A
             B   B
             C   C
         adata.var
-        >    pg
-         G1  G1
-         G2  G2
-         G3  G3
+        >    proteins
+         P1  'P1'
+         P2  'P2'
+         P3  'P3'
 
     If the observations are in the rows, you achieve the same behaviour by passing `row_type="observations"`
 
@@ -97,20 +102,78 @@ def read_pg_matrix(
            2        3   4   5  ...
            3        6   7   8  ...
 
-        adata = at.io.read_pg_matrix(pg_matrix_path, row_type="observations")
+        adata = at.io.read_pg_matrix(pg_matrix_path, row_type="observations", feature_name="proteins")
+
+    If the features or observations contain additional metadata, pass the column/row names as
+    `feature_metadata_index` or `sample_metadata_index` to the function to add them to the `.var`
+    respectively `.obs` attributes.
+
+    .. code-block:: python
+
+        # Inspect data
+        pd.read_csv(pg_matrix_path, sep="\t", index=0).head(3)
+        >                     1   2   3  ...
+           protein seq
+           P1       'AAA...'    0   1   2  ...
+           P2       'CCC...'    3   4   5  ...
+           P3       'DDD...'    6   7   8  ...
+
+        adata = at.io.read_pg_matrix(pg_matrix_path, row_type="features", feature_metadata_index=["protein", "seq"])
+        adata
+        > AnnData object with n_obs x n_vars = 3 x 3
+          obs: 'sample_id'
+          var: 'protein' 'seq'
+        adata.obs
+        >       sample_id
+            A   A
+            B   B
+            C   C
+        adata.var
+        >    protein seq
+         P1  'P1' 'AAA'
+         P2  'P2' 'CCC'
+         P3  'P3' 'DDD'
+
     """
+    _validate_row_type(row_type=row_type)
+
+    if isinstance(sample_metadata_index, str | int):
+        sample_metadata_index = [sample_metadata_index]
+    if isinstance(feature_metadata_index, str | int):
+        feature_metadata_index = [feature_metadata_index]
+
+    if isinstance(sample_name, str):
+        sample_name = [sample_name]
+    if isinstance(feature_name, str):
+        feature_name = [feature_name]
+
+    # Map metadata indices depending on orientation of dataframe
+    # to keep the API focussed on feature/sample syntax
+    if row_type == "features":
+        index_col = feature_metadata_index
+        header = sample_metadata_index
+    else:
+        index_col = sample_metadata_index
+        header = feature_metadata_index
+
     df = pd.read_csv(file_path, sep=separator, index_col=index_col, header=header, **kwargs)
 
     # Expects (observations x features)
     if row_type == "features":
         df = df.T
 
-    df = df.rename_axis(index=sample_name, columns=feature_name)
+    if sample_name is not None:
+        df = df.rename_axis(index=sample_name)
 
+    if feature_name is not None:
+        df = df.rename_axis(columns=feature_name)
+
+    # X needs to be an array
     X = df.to_numpy()
+
     # Whether to set sample names and feature names as indices in anndata
     # Rename axis to prevent redundant naming with column names
-    obs = df.index.to_frame(index=set_index).rename_axis(index=None)
-    var = df.columns.to_frame(index=set_index).rename_axis(index=None)
+    obs = df.index.to_frame(index=False).rename_axis(index=None)
+    var = df.columns.to_frame(index=False).rename_axis(index=None)
 
     return ad.AnnData(X=X, obs=obs, var=var)
--- tests/io/test_protein_group_reader.py
+++ tests/io/test_protein_group_reader.py
@@ -10,54 +10,123 @@ from alphatools.io import read_pg_matrix
 
 @pytest.fixture(
     params=[
-        # Set index=True
-        {"feature_name": "pg", "sample_name": "sample_id", "set_index": True, "row_type": "features"},
-        # # Set index=True and custom sample/feature name
-        {"feature_name": "protein_group", "sample_name": "sample_name", "set_index": True, "row_type": "features"},
-        # Set index=False
-        {"feature_name": "protein_group", "sample_name": "sample_name", "set_index": False, "row_type": "features"},
-        # # Transposed matrix (observation/features)
-        {"feature_name": "protein_group", "sample_name": "sample_name", "set_index": True, "row_type": "observations"},
-        # Set index=False
-        {"feature_name": "protein_group", "sample_name": "sample_name", "set_index": False, "row_type": "observations"},
+        # Basic
+        {
+            "feature_name": ("pg",),
+            "sample_name": ("sample_id",),
+            "row_type": "features",
+            "sample_metadata_index": 0,
+            "feature_metadata_index": 0,
+            "sample_metadata": (("A",), ("B",), ("C",)),
+            "feature_metadata": (("P1",), ("P2",), ("P3",), ("P4",)),
+        },
+        # Custom sample/feature name
+        {
+            "feature_name": ("protein_group",),
+            "sample_name": ("sample_name",),
+            "row_type": "features",
+            "sample_metadata_index": 0,
+            "feature_metadata_index": 0,
+            "sample_metadata": (("A",), ("B",), ("C",)),
+            "feature_metadata": (("P1",), ("P2",), ("P3",), ("P4",)),
+        },
+        # Transposed matrix (observation/features)
+        {
+            "feature_name": ("protein_group",),
+            "sample_name": ("sample_name",),
+            "row_type": "observations",
+            "sample_metadata_index": 0,
+            "feature_metadata_index": 0,
+            "sample_metadata": (("A",), ("B",), ("C",)),
+            "feature_metadata": (("P1",), ("P2",), ("P3",), ("P4",)),
+        },
+        ## Additional metadata ##
+        # Only features
+        {
+            "feature_name": ("protein_id", "seq"),
+            "sample_name": ("sample_id",),
+            "row_type": "features",
+            "sample_metadata_index": [0],
+            "feature_metadata_index": [0, 1],
+            "sample_metadata": (("A",), ("B",), ("C",)),
+            "feature_metadata": (("P1", "AAA"), ("P2", "CCC"), ("P3", "DDD"), ("P4", "EEE")),
+        },
+        # Only samples
+        {
+            "feature_name": ("protein_id",),
+            "sample_name": ("sample_id", "patient_id"),
+            "row_type": "features",
+            "sample_metadata_index": [0, 1],
+            "feature_metadata_index": [0],
+            "sample_metadata": (("A", "p1"), ("B", "p2"), ("C", "p3")),
+            "feature_metadata": (("P1",), ("P2",), ("P3",), ("P4",)),
+        },
+        {
+            "feature_name": ("protein_id", "seq"),
+            "sample_name": ("sample_id", "patient_id"),
+            "row_type": "features",
+            "sample_metadata_index": [0, 1],
+            "feature_metadata_index": [0, 1],
+            "sample_metadata": (("A", "p1"), ("B", "p2"), ("C", "p3")),
+            "feature_metadata": (("P1", "AAA"), ("P2", "CCC"), ("P3", "DDD"), ("P4", "EEE")),
+        },
     ]
 )
 def pg_matrix(tmpdir, request) -> Generator[tuple[str, ad.AnnData, dict[str, str]], None, None]:
     """Generate protein group matrix, reference :class:`anndata.AnnData` object for various argument combinations"""
     tmppath = tmpdir / "pg.tsv"
 
+    feature_metadata_index = request.param["feature_metadata_index"]
+    sample_metadata_index = request.param["sample_metadata_index"]
     feature_name = request.param["feature_name"]
     sample_name = request.param["sample_name"]
-    set_index = request.param["set_index"]
     row_type = request.param["row_type"]
+    index = request.param["sample_metadata"]
+    columns = request.param["feature_metadata"]
 
     # Generate a obs x features matrix
     X = np.arange(12).reshape(3, 4)
-    index = ["A", "B", "C"]
-    columns = ["P1", "P2", "P3", "P4"]
 
     # Simulated protein group matrix of shape
-    df = pd.DataFrame(X, index=index, columns=columns)
+    df = pd.DataFrame(
+        X,
+        index=pd.MultiIndex.from_tuples(index, names=sample_name),
+        columns=pd.MultiIndex.from_tuples(columns, names=feature_name),
+    )
 
     if row_type == "features":
         df = df.T
 
-    df.to_csv(tmppath, sep="\t", index=True)
+    # Pandas writes index names as additional row, which does
+    # not correspond to the expected format of PG matrices.
+    # Make indices actual columns to prevent this
+    df.reset_index().to_csv(tmppath, sep="\t", index=False)
 
     # anndata sets string numerics "0", "1", ... as index per default
-    obs_index = index if set_index else list(map(str, range(len(index))))
-    obs = pd.DataFrame(index, index=obs_index, columns=[sample_name])
+    obs_index = list(map(str, range(len(index))))
+    # obs_columns = pd.MultiIndex.from_tuples(index, names=sample_name)
+    obs = pd.DataFrame(index, index=obs_index, columns=sample_name)
 
-    var_index = columns if set_index else list(map(str, range(len(columns))))
-    var = pd.DataFrame(columns, index=var_index, columns=[feature_name])
+    var_index = list(map(str, range(len(columns))))
+    # var_columns = pd.MultiIndex.from_tuples(index, names=feature_name)
+    var = pd.DataFrame(columns, index=var_index, columns=feature_name)
 
     # AnnData stores observations x features
     adata = ad.AnnData(X=X, obs=obs, var=var)
 
+    sample_name = sample_name[0] if len(sample_name) == 1 else sample_name
+    feature_name = feature_name[0] if len(feature_name) == 1 else feature_name
+
     yield (
         tmppath,
         adata,
-        {"feature_name": feature_name, "sample_name": sample_name, "set_index": set_index, "row_type": row_type},
+        {
+            "row_type": row_type,
+            "feature_metadata_index": feature_metadata_index,
+            "sample_metadata_index": sample_metadata_index,
+            "feature_name": feature_name,
+            "sample_name": sample_name,
+        },
     )
 
     tmppath.remove()
