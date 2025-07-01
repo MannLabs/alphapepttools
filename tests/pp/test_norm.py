7f8e731886fb077293965e342fdc29d7364b062f
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-07-01 23:17:14 +0200

    [Doc] Update fixture documentation and add description of the other returend data types

--- tests/pp/test_norm.py
+++ tests/pp/test_norm.py
@@ -8,7 +8,8 @@ from alphatools.pp.norm import _total_mean_normalization, _validate_strategies
 
 @pytest.fixture
 def all_equal_count_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Generate count data (samples, features) where all samples have the same intensity"""
+    """Generate count data (samples, features) where all samples have the same intensity,
+    the expected result and the expected normalization factors"""
     X = np.array([[1.0, 1.0], [2.0, 0.0], [0.0, 2.0]])
 
     assert X.sum(axis=1).all()
