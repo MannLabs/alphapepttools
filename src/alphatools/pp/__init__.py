d777bfdf77d89a56f0a9716d66a9a2c759132ce7
Vincenth Brennsteiner <brennsteiner@biochem.mpg.de>
2025-07-17 08:43:40 +0200

    Add new functions to api docs

--- src/alphatools/pp/__init__.py
+++ src/alphatools/pp/__init__.py
@@ -3,4 +3,4 @@ from .embeddings import pca
 from .impute import impute
 from .metadata import add_core_proteome_mask
 from .norm import normalize
-from .transform import nanlog
+from .transform import check_data_integrity, nanlog
