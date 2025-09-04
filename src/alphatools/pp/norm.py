db102442cf0933f5edcca781eef823059d84f1b4
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-09-04 10:43:55 +0200

    [Bug] Handle nan values

--- src/alphatools/pp/norm.py
+++ src/alphatools/pp/norm.py
@@ -39,8 +39,8 @@ def _total_mean_normalization(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]
         > array([1.93333333, 1.93333333, 1.93333333])
     """
     # Compute sample-wise means
-    total_counts = data.sum(axis=1)
-    norm_factor = total_counts.mean() / total_counts
+    total_counts = np.nansum(data, axis=1)
+    norm_factor = np.nanmean(total_counts) / total_counts
 
     return data * norm_factor.reshape(-1, 1), norm_factor
 
