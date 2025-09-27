edcd95daf113ab75e0a97b1a91c6fee0261b092f
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-09-27 03:05:32 +0200

    [API] Expose metrics module to users

--- src/alphatools/__init__.py
+++ src/alphatools/__init__.py
@@ -1,5 +1,5 @@
-from . import io, pl, pp, tl
+from . import io, metrics, pl, pp, tl
 
-__all__: list[str] = ["io", "pl", "pp", "tl"]
+__all__: list[str] = ["io", "metrics", "pl", "pp", "tl"]
 
 __version__ = "0.0.1-dev0"
