67170d622ae55554c17370dc97f61010210e2737
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-10-28 18:54:01 +0100

    [Refactor] Initialize available datasets from yaml

--- src/alphatools/data/datasets.py
+++ src/alphatools/data/datasets.py
@@ -5,7 +5,10 @@ from pathlib import Path
 from typing import Literal
 
 import pandas as pd
+import yaml
 from alphabase.tools.data_downloader import DataShareDownloader
+
+STUDY_CONFIG = Path(__file__).parent / "datasets.yaml"
 
 
 @dataclass
@@ -103,7 +106,11 @@ class StudyCollection:
     @property
     def df(self) -> pd.DataFrame:
         """Dataframe of available studies"""
-        return pd.concat([study.df for study in self.collection]) if len(self.collection) > 0 else pd.DataFrame()
+        return (
+            pd.concat([study.df for study in self.collection], ignore_index=True)
+            if len(self.collection) > 0
+            else pd.DataFrame()
+        )
 
     def __repr__(self):
         return self.df.__repr__()
@@ -112,72 +119,19 @@ class StudyCollection:
         """HTML representation for Jupyter notebooks"""
         return self.df.to_html()
 
+    @classmethod
+    def from_yaml(cls, file_path: str) -> "StudyCollection":
+        """Initialize class with studies from yaml file"""
+        with Path.open(file_path, "r") as file:
+            studies = yaml.safe_load(file.read())
 
-bader2020_pg_alphadia = StudyData(
-    name="bader2020_pg_alphadia",
-    url="https://datashare.biochem.mpg.de/s/yLpjkoQzMHpdDsB",
-    search_engine="alphadia",
-    citation="Bader JM, Geyer PE, Müller JB, Strauss MT, Koch M, Leypoldt F, Koertvelyessy P, Bittner D, Schipke CG, Incesoy EI, Peters O, Deigendesch N, Simons M, Jensen MK, Zetterberg H, Mann M. Proteome profiling in cerebrospinal fluid reveals novel biomarkers of Alzheimer's disease. Mol Syst Biol. 2020 Jun;16(6):e9356. doi: 10.15252/msb.20199356. PMID: 32485097; PMCID: PMC7266499.",
-    data_type="study_pg",
-    description="Study on Cerebral Spinal Fluid of Alzheimer patients by Bader et al.",
-)
+        studies = [StudyData(**study) for study in studies]
 
-bader2020_pg_diann = StudyData(
-    name="bader2020_pg_diann",
-    url="https://datashare.biochem.mpg.de/s/3oZsya2L5bGnmtQ",
-    search_engine="DiaNN",
-    citation="Bader JM, Geyer PE, Müller JB, Strauss MT, Koch M, Leypoldt F, Koertvelyessy P, Bittner D, Schipke CG, Incesoy EI, Peters O, Deigendesch N, Simons M, Jensen MK, Zetterberg H, Mann M. Proteome profiling in cerebrospinal fluid reveals novel biomarkers of Alzheimer's disease. Mol Syst Biol. 2020 Jun;16(6):e9356. doi: 10.15252/msb.20199356. PMID: 32485097; PMCID: PMC7266499.",
-    data_type="study_pg",
-    description="Study on Cerebral Spinal Fluid of Alzheimer patients by Bader et al.",
-)
-
-spectronaut_pg = StudyData(
-    name="spectronaut_pg",
-    url="https://datashare.biochem.mpg.de/s/Ai9TiBTeaPHK5by",
-    search_engine="spectronaut",
-    data_type="pg",
-    description="An example spectronaut report",
-)
-
-bader2020_psm_alphadia = StudyData(
-    name="bader2020_psm_alphadia",
-    url="https://datashare.biochem.mpg.de/s/awYyxod4ksz86kk",
-    search_engine="alphadia",
-    citation="Bader JM, Geyer PE, Müller JB, Strauss MT, Koch M, Leypoldt F, Koertvelyessy P, Bittner D, Schipke CG, Incesoy EI, Peters O, Deigendesch N, Simons M, Jensen MK, Zetterberg H, Mann M. Proteome profiling in cerebrospinal fluid reveals novel biomarkers of Alzheimer's disease. Mol Syst Biol. 2020 Jun;16(6):e9356. doi: 10.15252/msb.20199356. PMID: 32485097; PMCID: PMC7266499.",
-    data_type="study_psm",
-    description="Study on Cerebral Spinal Fluid of Alzheimer patients by Bader et al.",
-)
-
-bader2020_psm_diann = StudyData(
-    name="bader2020_psm_diann",
-    url="https://datashare.biochem.mpg.de/s/c4Z5Yg6srKQyDym",
-    search_engine="DiaNN",
-    citation="Bader JM, Geyer PE, Müller JB, Strauss MT, Koch M, Leypoldt F, Koertvelyessy P, Bittner D, Schipke CG, Incesoy EI, Peters O, Deigendesch N, Simons M, Jensen MK, Zetterberg H, Mann M. Proteome profiling in cerebrospinal fluid reveals novel biomarkers of Alzheimer's disease. Mol Syst Biol. 2020 Jun;16(6):e9356. doi: 10.15252/msb.20199356. PMID: 32485097; PMCID: PMC7266499.",
-    data_type="study_psm",
-    description="Study on Cerebral Spinal Fluid of Alzheimer patients by Bader et al.",
-)
-
-spectronaut_psm = StudyData(
-    name="spectronaut_psm",
-    url="https://datashare.biochem.mpg.de/s/GtfJL49Rf9w78EE",
-    search_engine="spectronaut",
-    data_type="study_psm",
-    description="An example spectronaut report",
-)
+        return cls().register(studies)
 
 
 # Initialize class and register all available studies
-study_collection = StudyCollection()
-study_collection.register(
-    [
-        bader2020_pg_alphadia,
-        bader2020_pg_diann,
-        spectronaut_pg,
-        bader2020_psm_alphadia,
-        bader2020_psm_diann,
-        spectronaut_psm,
-    ]
-)
+study_collection = StudyCollection.from_yaml(STUDY_CONFIG)
 
 
 def available_data() -> StudyCollection:
