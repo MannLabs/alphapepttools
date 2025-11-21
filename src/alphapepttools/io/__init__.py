6bfa3d08a93ecb58bce9b6f20852fb2f30c2f654
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-11-21 14:57:08 +0100

    [API] Expose get_available_reader to users

--- src/alphapepttools/io/__init__.py
+++ src/alphapepttools/io/__init__.py
@@ -1,5 +1,6 @@
 from .anndata_factory import AnnDataFactory
 from .pg_reader import read_pg_table
 from .psm_reader import read_psm_table
+from .utils import available_reader
 
-__all__ = ["AnnDataFactory", "read_pg_table", "read_psm_table"]
+__all__ = ["AnnDataFactory", "available_reader", "read_pg_table", "read_psm_table"]
