dfd82b6e208a46ccbfd19b4a9fd882d74d17102c
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-11-21 17:34:28 +0100

    [Refactor] Rename parameter from reader_type to kind for consistency in list_available_reader function

--- src/alphapepttools/io/utils.py
+++ src/alphapepttools/io/utils.py
@@ -6,12 +6,12 @@ from alphabase.pg_reader import pg_reader_provider
 from alphabase.psm_reader import psm_reader_provider
 
 
-def list_available_reader(reader_type: Literal["psm_reader", "pg_reader"] = "pg_reader") -> list[str]:
+def list_available_reader(kind: Literal["psm_reader", "pg_reader"] = "pg_reader") -> list[str]:
     """Get a list of all available readers, as provided by alphabase
 
     Parameters
     ----------
-    reader_type
+    kind
         Whether to return readers for peptice spectrum matches (`psm_reader`) or protein group
         intensities (`pg_reader`)
 
@@ -29,8 +29,8 @@ def list_available_reader(reader_type: Literal["psm_reader", "pg_reader"] = "pg_
         > ['alphadia', 'alphapept', ...]
 
     """
-    if reader_type == "psm_reader":
+    if kind == "psm_reader":
         return sorted(psm_reader_provider.reader_dict.keys())
-    if reader_type == "pg_reader":
+    if kind == "pg_reader":
         return sorted(pg_reader_provider.reader_dict.keys())
-    raise KeyError(f"Pass either `psm_reader` or `pg_reader`, not {reader_type}")
+    raise KeyError(f"Pass either `psm_reader` or `pg_reader`, not {kind}")
--- tests/io/test_utils.py
+++ tests/io/test_utils.py
@@ -4,15 +4,15 @@ import pytest
 from alphabase.pg_reader import pg_reader_provider
 from alphabase.psm_reader import psm_reader_provider
 
-from alphapepttools.io import available_reader
+from alphapepttools.io import list_available_reader
 
 
-@pytest.mark.parametrize("reader_type", ["psm_reader", "pg_reader"])
-def test_available_reader(reader_type: Literal["psm_reader", "pg_reader"]) -> None:
-    list_of_available_reader = available_reader(reader_type)
+@pytest.mark.parametrize("kind", ["psm_reader", "pg_reader"])
+def test_available_reader(kind: Literal["psm_reader", "pg_reader"]) -> None:
+    list_of_available_reader = list_available_reader(kind)
 
-    if reader_type == "psm_reader":
+    if kind == "psm_reader":
         assert len(list_of_available_reader) == len(psm_reader_provider.reader_dict)
-    elif reader_type == "pg_reader":
+    elif kind == "pg_reader":
         assert len(list_of_available_reader) == len(pg_reader_provider.reader_dict)
     assert "alphadia" in list_of_available_reader
