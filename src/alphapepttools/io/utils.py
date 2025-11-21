1a3e3ec248dd41b49b66b3bb7b2367b0aedfb5d4
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-11-21 14:56:47 +0100

    [Feature] Add  function to alphatools

---
+++ src/alphapepttools/io/utils.py
@@ -0,0 +1,22 @@
+"""Utility functions for readers"""
+
+from typing import Literal
+
+from alphabase.pg_reader import pg_reader_provider
+from alphabase.psm_reader import psm_reader_provider
+
+
+def available_reader(reader_type: Literal["psm_reader", "pg_reader"] = "psm_reader") -> list[str]:
+    """Get a list of all available readers, as provided by alphabase
+
+    Parameters
+    ----------
+    reader_type
+        Whether to return readers for peptice spectrum matches (`psm_reader`) or protein group
+        intensities (`pg_reader`)
+    """
+    if reader_type == "psm_reader":
+        return sorted(psm_reader_provider.reader_dict.keys())
+    if reader_type == "pg_reader":
+        return sorted(pg_reader_provider.reader_dict.keys())
+    raise KeyError(f"Pass either `psm_reader` or `pg_reader`, not {reader_type}")
