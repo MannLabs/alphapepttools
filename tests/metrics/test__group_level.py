36a70f997f43a038cf3eea8477f5aa413d73550a
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-11-26 14:08:29 +0100

    [Refactor] Refactor to class-based tests

--- tests/metrics/test__group_level.py
+++ tests/metrics/test__group_level.py
@@ -16,65 +16,73 @@ def count_data_pmad() -> tuple[np.ndarray, float]:
     return X, 3.0
 
 
-@pytest.fixture
-def adata_pmad(count_data_pmad) -> tuple[np.ndarray, float]:
-    """Generate count data with known PMAD"""
-    # Concatenate the same count matrix with known PMADs for 3 different sample groups
-    X, pmad = count_data_pmad
-    n_obs = X.shape[0]
-
-    sample_types = ["A"] * n_obs + ["B"] * n_obs + ["C"] * n_obs
-    X = np.concatenate([X for _ in range(3)], axis=0)
-
-    adata = ad.AnnData(X=X, layers={"layer": X}, obs=pd.DataFrame({"sample_type": sample_types}))
-
-    return {"adata": adata, "pmad": {"A": pmad, "B": pmad, "C": pmad}, "group_key": "sample_type"}
-
-
-@pytest.mark.parametrize(
-    ("dictionary", "keys", "value", "reference"),
-    [
-        # Initial test
-        ({}, ["key1"], "value", {"key1": "value"}),
-        # Do not overwrite existing keys
-        ({"existing_key": "existing_value"}, ["key1"], "value", {"existing_key": "existing_value", "key1": "value"}),
-        # Multiple keys
-        ({}, ["key1", "key2"], "value", {"key1": {"key2": "value"}}),
-        # Write non-string values
-        ({}, ["key1", "key2"], [], {"key1": {"key2": []}}),
-    ],
-)
-def test__set_nested_dict(dictionary: dict[str, Any], value: Any, keys: list[str], reference: dict[str, Any]) -> None:
-    """Test recursively setting dictionary keys in a dictionary"""
-    result = _set_nested_dict(dictionary=dictionary, keys=keys, value=value)
-
-    assert result == reference
-
-
-def test__pmad(count_data_pmad) -> None:
-    X, pmad = count_data_pmad
-
-    assert _pmad(x=X) == pmad
-
-
-@pytest.mark.parametrize("layer", [None, "layer"])
-def test_pooled_median_absolute_deviation_return(adata_pmad, layer) -> None:
-    """Test if `pooled_median_absolute_deviation` computes group-wise PMAD correctly"""
-    reference = pd.DataFrame.from_dict(adata_pmad["pmad"], orient="index", columns=["pmad"])
-
-    pmad = pooled_median_absolute_deviation(
-        adata_pmad["adata"], group_key=adata_pmad["group_key"], layer=layer, inplace=False
+class TestSetNestedDict:
+    @pytest.mark.parametrize(
+        ("dictionary", "keys", "value", "reference"),
+        [
+            # Initial test
+            ({}, ["key1"], "value", {"key1": "value"}),
+            # Do not overwrite existing keys
+            (
+                {"existing_key": "existing_value"},
+                ["key1"],
+                "value",
+                {"existing_key": "existing_value", "key1": "value"},
+            ),
+            # Multiple keys
+            ({}, ["key1", "key2"], "value", {"key1": {"key2": "value"}}),
+            # Write non-string values
+            ({}, ["key1", "key2"], [], {"key1": {"key2": []}}),
+        ],
     )
+    def test__set_nested_dict(
+        self, dictionary: dict[str, Any], value: Any, keys: list[str], reference: dict[str, Any]
+    ) -> None:
+        """Test recursively setting dictionary keys in a dictionary"""
+        result = _set_nested_dict(dictionary=dictionary, keys=keys, value=value)
 
-    assert pmad.equals(reference)
+        assert result == reference
 
 
-@pytest.mark.parametrize("layer", [None, "layer"])
-def test_pooled_median_absolute_deviation_inplace(adata_pmad, layer) -> None:
-    """Test if `pooled_median_absolute_deviation` sets PMAD correctly in anndata object"""
-    reference = adata_pmad["pmad"]
-    adata = adata_pmad["adata"].copy()
+class TestPmad:
+    def test__pmad(self, count_data_pmad) -> None:
+        X, pmad = count_data_pmad
 
-    pooled_median_absolute_deviation(adata, group_key=adata_pmad["group_key"], layer=layer, inplace=True)
+        assert _pmad(x=X) == pmad
 
-    assert adata.uns.get("metrics").get("pmad") == reference
+
+class TestPoooledMedianAbsoluteDeviation:
+    @pytest.fixture
+    def adata_pmad(self, count_data_pmad) -> tuple[np.ndarray, float]:
+        """Generate count data with known PMAD"""
+        # Concatenate the same count matrix with known PMADs for 3 different sample groups
+        X, pmad = count_data_pmad
+        n_obs = X.shape[0]
+
+        sample_types = ["A"] * n_obs + ["B"] * n_obs + ["C"] * n_obs
+        X = np.concatenate([X for _ in range(3)], axis=0)
+
+        adata = ad.AnnData(X=X, layers={"layer": X}, obs=pd.DataFrame({"sample_type": sample_types}))
+
+        return {"adata": adata, "pmad": {"A": pmad, "B": pmad, "C": pmad}, "group_key": "sample_type"}
+
+    @pytest.mark.parametrize("layer", [None, "layer"])
+    def test_pooled_median_absolute_deviation_return(self, adata_pmad: ad.AnnData, layer: str | None) -> None:
+        """Test if `pooled_median_absolute_deviation` computes group-wise PMAD correctly"""
+        reference = pd.DataFrame.from_dict(adata_pmad["pmad"], orient="index", columns=["pmad"])
+
+        pmad = pooled_median_absolute_deviation(
+            adata_pmad["adata"], group_key=adata_pmad["group_key"], layer=layer, inplace=False
+        )
+
+        assert pmad.equals(reference)
+
+    @pytest.mark.parametrize("layer", [None, "layer"])
+    def test_pooled_median_absolute_deviation_inplace(self, adata_pmad: ad.AnnData, layer: str | None) -> None:
+        """Test if `pooled_median_absolute_deviation` sets PMAD correctly in anndata object"""
+        reference = adata_pmad["pmad"]
+        adata = adata_pmad["adata"].copy()
+
+        pooled_median_absolute_deviation(adata, group_key=adata_pmad["group_key"], layer=layer, inplace=True)
+
+        assert adata.uns.get("metrics").get("pmad") == reference
