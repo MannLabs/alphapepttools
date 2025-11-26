df654e77e9c7447e71cd424a4d25145ca4b81437
Lucas Diedrich <diedrich@biochem.mpg.de>
2025-11-26 14:41:17 +0100

    [API] Expose PCV

--- src/alphapepttools/metrics/__init__.py
+++ src/alphapepttools/metrics/__init__.py
@@ -1,7 +1,12 @@
 """Metrics for the quality assessment of the analysis"""
 
 from ._feature_level import coefficient_of_variation
-from ._group_level import pooled_median_absolute_deviation
+from ._group_level import pooled_coefficient_of_variation, pooled_median_absolute_deviation
 from ._principal_component_regression import principal_component_regression
 
-__all__ = ["coefficient_of_variation", "pooled_median_absolute_deviation", "principal_component_regression"]
+__all__ = [
+    "coefficient_of_variation",
+    "pooled_coefficient_of_variation",
+    "pooled_median_absolute_deviation",
+    "principal_component_regression",
+]
