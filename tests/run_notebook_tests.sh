82042dfafa5a0693c900527092079295f52468ef
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-02-03 20:21:30 +0100

    Fix naming

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -5,6 +5,6 @@
 export IS_PYTEST_RUN=True
 
 # TODO enable also study_03_biomarker_skin.ipynb
-ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v -e "study_03_biomarker_skin" -e "supplementary_02_scalability-demo_PCAn workflow")
+ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v -e "study_03_biomarker_skin" -e "supplementary_02_scalability-demo_PCAn")
 
 python -m pytest --nbmake $(echo $ALL_NBS)
