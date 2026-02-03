042f69d6ae365adcc727f610d09b27a5af430f76
Vincenth Brennsteiner <brennsteiner@biochem.mpg.de>
2026-02-03 18:44:37 +0100

    exclude study_04_scDVP.ipynb from tests

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -5,6 +5,6 @@
 export IS_PYTEST_RUN=True
 
 # TODO enable also study_03_biomarker_skin.ipynb
-ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v "study_03_biomarker_skin")
+ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v -e "study_03_biomarker_skin" -e "study_04_scDVP.ipynb")
 
 python -m pytest --nbmake $(echo $ALL_NBS)
