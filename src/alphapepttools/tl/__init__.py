05a415e62068273885b399c84319c2c3e860f0db
Vincenth Brennsteiner <brennsteiner@biochem.mpg.de>
2026-02-05 15:11:58 +0100

    finish first review round of study_04 notebook

--- src/alphapepttools/tl/__init__.py
+++ src/alphapepttools/tl/__init__.py
@@ -3,5 +3,6 @@ from .diff_exp.alphaquant import diff_exp_alphaquant
 from .diff_exp.ebayes import diff_exp_ebayes
 from .diff_exp.ttest import diff_exp_ttest, nan_safe_ttest_ind
 from .embeddings import bpca, pca
+from .plot_data_handling import prepare_pca_1d_loadings_data_to_plot
 from .stats import nan_safe_bh_correction
 from .tools import get_id2gene_map, map_genes_to_protein_groups
