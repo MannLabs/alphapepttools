4cdac3b6e2f5b664cc168d50d2b40c0e1a3dbebb
ShaniBeMo <benmoshe@biochem.mpg.de>
2026-02-24 17:54:51 +0100

    update tutorial nb and metrics init file

--- src/alphapepttools/metrics/__init__.py
+++ src/alphapepttools/metrics/__init__.py
@@ -1,16 +1,22 @@
 """Metrics for the quality assessment of the analysis"""
 
-from ._feature_level import calculate_qc_metrics, coefficient_of_variation, frac_detected, num_detected, sum_intensity
+from ._feature_level import (
+    calculate_qc_metrics,
+    coefficient_of_variation,
+    fraction_complete,
+    num_features_detected,
+    total_intensity,
+)
 from ._group_level import pooled_coefficient_of_variation, pooled_median_absolute_deviation
 from ._principal_component_regression import principal_component_regression
 
 __all__ = [
     "calculate_qc_metrics",
     "coefficient_of_variation",
-    "frac_detected",
-    "num_detected",
+    "fraction_complete",
+    "num_features_detected",
     "pooled_coefficient_of_variation",
     "pooled_median_absolute_deviation",
     "principal_component_regression",
-    "sum_intensity",
+    "total_intensity",
 ]
