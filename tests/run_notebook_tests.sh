7731ceedcdd718d184f6824b7e4af1aff4db90df
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-02-24 16:14:52 +0100

    Remove inmoose check as notebooks are now only tested in a full environment.

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -4,13 +4,9 @@
 
 export IS_PYTEST_RUN=True
 
-# TODO enable also study_03_biomarker_skin.ipynb
+# study_03_biomarker_skin is excluded as the data is not publicly available yet
+# TODO: enable also study_03_biomarker_skin.ipynb
 EXCLUDE_PATTERN="study_03_biomarker_skin"
-
-# Check if inmoose is available, exclude study_02_peptidomics_pelsa if not
-if ! python -c "import inmoose" 2>/dev/null; then
-    EXCLUDE_PATTERN="study_03_biomarker_skin|study_02_peptidomics_pelsa"
-fi
 
 ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -vE "$EXCLUDE_PATTERN")
 
