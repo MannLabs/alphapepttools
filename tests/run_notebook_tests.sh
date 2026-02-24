0786f8b9ba27376b41a3e3c05d0f2475702bd33f
ShaniBeMo <benmoshe@biochem.mpg.de>
2026-02-24 16:27:12 +0100

    add pelsa nb to run_notebook_texts

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -4,7 +4,7 @@
 
 export IS_PYTEST_RUN=True
 
-ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v -e "study_03_biomarker_skin" -e "study_04_scDVP.ipynb")
+ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -v -e "study_03_biomarker_skin" -e "study_04_scDVP.ipynb" -e "study_02_peptidomics_pelsa.ipynb")
 
 
 python -m pytest --nbmake $(echo $ALL_NBS)
