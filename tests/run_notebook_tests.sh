dd446387846bf5ef1e177ff718455b45f863a1a5
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-03-05 09:40:19 +0100

    [Fix] Exclude scDVP notebook from tests

--- tests/run_notebook_tests.sh
+++ tests/run_notebook_tests.sh
@@ -5,8 +5,9 @@
 export IS_PYTEST_RUN=True
 
 # study_03_biomarker_skin is excluded as the data is not publicly available yet
+# study_04_scdvp is excluded as it includes decoupler as dependency for the downstream analysis, which is not needed for the package
 # TODO: enable also study_03_biomarker_skin.ipynb
-EXCLUDE_PATTERN="study_03_biomarker_skin"
+EXCLUDE_PATTERN="study_03_biomarker_skin|study_04_scDVP"
 
 ALL_NBS=$(find ../docs/notebooks -name "*.ipynb" | grep -vE "$EXCLUDE_PATTERN")
 
