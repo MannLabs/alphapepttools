e7175550829296b3aa8716dedbf404fb853a461e
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-09-03 14:24:00 +0200

    Update return signature of pooled_coefficient_of_variation

--- src/alphapepttools/metrics/feature_level.py
+++ src/alphapepttools/metrics/feature_level.py
@@ -383,6 +383,7 @@ def pooled_coefficient_of_variation(
 
     if inplace:
         adata.uns = _set_nested_dict(adata.uns, value=pcv_groupwise, keys=[METRICS_KEY, PCV_KEY])
+        return None
     return pd.DataFrame.from_dict(pcv_groupwise, orient="index", columns=pd.Index([PCV_KEY]))
 
 
